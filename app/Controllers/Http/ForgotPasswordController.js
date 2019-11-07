e73de73e437d1518df670170754db5232b489317
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-11-07 14:13:42 -0300

    crud, models, antl, pagination and task hook

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -29,6 +30,34 @@ class ForgotPasswordController {
       return response
         .status(err.status)
         .send({ error: { message: 'Something happen. This email really exists?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'This token was expired. Request a new Password Recover' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Error at updating your password' } })
     }
   }
 }
