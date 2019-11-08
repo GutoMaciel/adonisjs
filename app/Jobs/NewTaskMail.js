840a3bd0c785539b4e12aa1be1c9b44257f44cb8
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-11-08 13:40:39 -0300

    final ajustments at backend

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`)
+    await Mail.send(
+      ['emails.new_task'],
+      { username, title, hasAttachment: !!file },
+      message => {
+        message
+          .to(email)
+          .from('guto@digitaltork.com', 'Gustavo | Digital Tork')
+          .subject('You have a new task')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 
 const TaskHook = exports = module.exports = {}
 
@@ -13,20 +13,5 @@ TaskHook.sendNewTaskMail = async taskInstance => {
 
   const { title } = taskInstance
 
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from('guto@digitaltork.com', 'Gustavo | Digital Tork')
-        .subject('You have a new task')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { email, username, file, title }, { attemps: 3 })
 }
